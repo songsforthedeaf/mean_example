0d74b22875019dc84f4f8cc18568aa08dcd86514
Anil Ganti <Anil@Portia.local>
2017-02-04 10:53:12 -0500

    add .gitignore and listen on port 5000

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require('express')
- 
 var app = express()
- 
+
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/notes', function(req, res) {
   res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
 })
  
-app.listen(3000)
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
